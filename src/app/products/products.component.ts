55e1298a9ea6267dae90ed1e4cba3d0e107cff19
Andell Mohan <andell.mohan@gmail.com>
2017-06-21 07:38:51 +0200

    first commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+  products = [
+    { "title": 'USB Car Charger', "description": "My description here" }
+  ];
+
+  title:string = '';
+  description:string = '';
+
+  addProduct(value:any) {
+    this.products.push({'title': value.title, 'description':value.description});
+    this.title = '';
+    this.description = '';
+  }
+
+  removeProduct(product) {
+    this.products.splice(product, 1);
+  }
+}
